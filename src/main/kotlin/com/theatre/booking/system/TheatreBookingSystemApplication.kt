1c2e69319bfea5d411cbd41992b3c1842c68580b
Abhilash Kale <abhilash.kale@go-jek.com>
2021-08-31 21:31:36 +0530

    2 - Added basic controller with hellor world template and gradle dependencies

---
+++ src/main/kotlin/com/theatre/booking/system/TheatreBookingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.theatre.booking.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheatreBookingSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheatreBookingSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/theatre/booking/system/TheatreBookingSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.theatre.booking.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheatreBookingSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
